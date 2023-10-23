737cc151cbb7c3d89a33035d103f974a97772914
fahdaroua <fahd.aroua@gmail.com>
2023-10-23 21:17:33 +0200

    updated

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,6 @@
 
-//uncomment to use the model from mongodb-database
-// const Book = require("../mongo-database/models/book")
+
+const Book = require("../mongo-database/models/book")
 
 // uncomment to use mysql models
 // const {getAll,createOne,updateOne,deleteOne,deleteAll,getOne} = require("../mysql-database/models/Book")
@@ -8,16 +8,16 @@
 module.exports = {
 
     getOne:async(req,res)=>{
-        // uncomment if using mongoDb
+       
 
-        // try{
-        //     const data = await Book.findOne({_id:req.params.id})
-        //     res.status(200).json(data)
+        try{
+            const data = await Book.findOne({_id:req.params.id})
+            res.status(200).json(data)
 
-        // }
-        // catch(error) {
-        //     res.status(500).json(error)
-        // }
+        }
+        catch(error) {
+            res.status(500).json(error)
+        }
 
         // uncomment if using mysql
 
@@ -31,17 +31,46 @@ module.exports = {
         // }
         
     },
-    createOne :  async (req,res)=> {  
+    createOne: async (req, res) => {
+        try {
+            const { name, author, category, numberOfPages } = req.body;
+            const newBook = new Book({ name, author, category, numberOfPages });
+            await newBook.save();
+            res.status(201).json(newBook); 
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
-    getAll : async (req,res) => {
+    getAll: async (req, res) => {
+        try {
+            const books = await Book.find();
+            res.status(200).json(books);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
-    updateOne : async (req,res) => {
-
+    updateOne: async (req, res) => {
+        try {
+            const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            res.status(200).json(updatedBook);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
-    deleteOne : async (req,res)=> {
-
+    deleteOne: async (req, res) => {
+        try {
+            await Book.findByIdAndDelete(req.params.id);
+            res.status(200).json({ message: 'Book deleted successfully' });
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
-    deleteAll : async(req,res) => {
-
+    deleteAll: async (req, res) => {
+        try {
+            await Book.deleteMany();
+            res.status(200).json({ message: 'All books deleted successfully' });
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-}
+};
--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router  = express.Router();
-const {createOne,updateOne,deleteOne,deleteAll,getOne} = require("../controllers/books.js")
+const {createOne,updateOne,deleteOne,deleteAll,getOne,getAll} = require("../controllers/books.js")
 
 router.get("/getoneBook/:id",getOne)
-router.get("/getAllBooks",()=>{
-    
-})
-// implement other routes ...
+
+router.get('/getAllBooks', getAll); 
+router.post('/createBook', createOne); 
+router.put('/updateBook/:id', updateOne); 
+router.delete('/deleteOneBook/:id', deleteOne);
+router.delete('/deleteAllBooks', deleteAll); 
 
 module.exports = router;
+
+
